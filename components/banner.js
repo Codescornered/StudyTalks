f8e67c3879d61dd36188e4188e4b9d38d3be104d
Samrudh Keerthi <Sam@MacBook-Pro.local>
2023-04-26 21:00:08 +0530

    commit to fix preloader glitch

--- components/banner.js
+++ components/banner.js
@@ -1,6 +1,6 @@
 import { useSession, useSupabaseClient } from "@supabase/auth-helpers-react";
 import { useState } from "react";
-import Preloader from "./Preloader";
+import Preloader from "./preloader";
 import { uploadUserImage } from "../helpers/user";
 
 export default function Cover({ url, editable, onChange }) {
